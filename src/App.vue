a5b75e077c333500ccb507129d4fe86944f36c14
Skrollan Elena Zimmermann <skrollanelenazimmermann@gmail.com>
2023-04-30 02:19:58 +0200

    Clear beginning

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Logo" src="./assets/HiTwitch.png" style="width: 15%; height: auto;">
+  <HelloWorld/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import myBackground from '@/assets/background.jpg';
+import HiTwitch from '@/assets/HiTwitch.png';
+
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      imageHiTwitch: HiTwitch
+    }
+  },
+  mounted() {
+    document.body.style.backgroundColor = '#16a085';
+    document.body.style.backgroundImage = `url(${myBackground})`;
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Comfortaa', cursive;
   text-align: center;
-  color: #2c3e50;
+  color: rgb(228, 228, 228);
   margin-top: 60px;
 }
 </style>
