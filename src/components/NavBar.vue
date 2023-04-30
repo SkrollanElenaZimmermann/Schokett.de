f289f5294941067dd4501fb655e35a2e1264b156
Skrollan Elena Zimmermann <skrollanelenazimmermann@gmail.com>
2023-04-30 03:36:29 +0200

    style navbar buttons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -28,21 +28,24 @@ export default {
  align-items: center;
  font-size: 20px;
 }
-
 .navbar .logo {
   padding: 20px 10px;
 }
-
 .navbar .nav_buttons {
   display: flex;
 }
-
-.navbar .nav_buttons .btn {
+.navbar .nav_buttons a {
   background-color: rgb(122, 122, 122);
   color: white;
   padding: 10px 20px;
   margin: 0 10px;
   cursor: pointer;
+  text-decoration: none;
+}
+.active {
+  color: rgb(0, 255, 200) !important;
+  background-color: rgb(155, 155, 155) !important;
+  box-shadow: 1px 2px rgb(0, 255, 200) !important;
 }
 
 </style>
